63bff76bfaa7514a6fa576ca911cc606d36604ee
Victor Manuel Garcia <victor.garcia@infracommerce.lat>
2023-04-11 09:32:51 -0500

    actuailzacion index para controladores login

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 
 const ActionsController = require('./App/Http/Controllers/ActionsController')
 const ActionsTypesController = require('./App/Http/Controllers/ActionsTypesController')
+const CronsController = require('./App/Http/Controllers/CronsController')
 const HistoryFlowController = require('./App/Http/Controllers/HistoryFlowController')
 const InputsUpdatesController = require('./App/Http/Controllers/InputsUpdatesController')
+const LoginTypesController = require('./App/Http/Controllers/LoginTypesController')
 const NodesFlowsController = require('./App/Http/Controllers/NodesFlowsController')
+const OauthController = require('./App/Http/Controllers/OauthController')
 const SettingsController = require('./App/Http/Controllers/SettingsController')
 const SourcesController = require('./App/Http/Controllers/SourcesController')
-const CronsController = require('./App/Http/Controllers/CronsController')
 
 module.exports = {
   ActionsController,
   ActionsTypesController,
+  CronsController,
   HistoryFlowController,
   InputsUpdatesController,
+  LoginTypesController,
   NodesFlowsController,
+  OauthController,
   SettingsController,
-  SourcesController,
-  CronsController
+  SourcesController
 };
