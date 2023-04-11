420a418f4422e55bc4505ce2176c490feef85411
Victor Manuel Garcia <victor.garcia@infracommerce.lat>
2023-04-11 11:29:58 -0500

    actualizacion modelos

--- App/Model.js
+++ App/Model.js
@@ -1,2 +1,6 @@
-const Model = require('./Connection/mysql/Model')
+require('dotenv').config();
+const env = process.env;
+const DB_CONNECTION = env.DB_CONNECTION ? env.DB_CONNECTION : 'mysql'; 
+
+const Model = require('./Connection/'+DB_CONNECTION+'/Model')
 module.exports = Model;
